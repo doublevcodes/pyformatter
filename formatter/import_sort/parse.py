1733d8cf047adc8e13eae6ffb4625cc81aab5e6e
Vivaan Verma <54081925+doublevcodes@users.noreply.github.com>
2021-07-18 23:44:39 +0100

    Update parse.py

--- formatter/import_sort/parse.py
+++ formatter/import_sort/parse.py
@@ -1,33 +1,31 @@
-from __future__ import barry_as_FLUFL
+import ast
+from io import TextIOWrapper
+from itertools import groupby
+from typing import Generator
 
-from __future__ import nested_scopes
 from formatter.import_sort.core import (
     ImportType,
     ImportTypeChecker
 )
-import ast
-from rich.pretty import pprint
-
-from io import TextIOWrapper
-
-from flask import Flask
-from itertools import groupby
 
 
 class ImportParser:
 
-    def __init__(self, filename: str) -> None:
-        self.filename: str = filename
-        self.import_cache = None
+    ImportFormatType = list[tuple[str, tuple[str, ImportType]]]
 
-    def parse(self):
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+        self.import_cache = None
+        return None
+
+    def parse(self) -> str:
         self.get_top_imports()
         self.sort_imports()
         self.construct_new_imports()
         return self.import_cache
 
     @staticmethod
-    def _import_gen(file: str):
+    def _import_gen(file: str) -> Generator[tuple[str, str], None, None]:
         for node in ast.iter_child_nodes(ast.parse(file)):
             if isinstance(node, ast.Import):
                 yield node.names[0].name, ast.unparse(node)
@@ -35,31 +33,30 @@ class ImportParser:
                 yield node.module, ast.unparse(node)
 
 
-    def get_top_imports(self):
+    def get_top_imports(self) -> None:
         file: TextIOWrapper = open(self.filename)
-        imports = [(imp, (imp_str, ImportTypeChecker.get_import_type(imp))) for imp, imp_str in self._import_gen(file.read())]
+        imports: ImportFormatType = [
+            (imp, (imp_str, ImportTypeChecker.get_import_type(imp)))
+            for imp, imp_str in self._import_gen(file.read())
+        ]
         self.import_cache = imports
-        return
+        return None
 
     @staticmethod
-    def sort_key(imp):
+    def sort_key(imp) -> int:
         return imp[1][1].value
 
-    def sort_imports(self):
-        list_of_imports = sorted(self.import_cache, key=ImportParser.sort_key)
+    def sort_imports(self) -> None:
+        list_of_imports: ImportFormatType = sorted(self.import_cache, key=ImportParser.sort_key)
         section_imports = [list(v) for _, v in groupby(list_of_imports, ImportParser.sort_key)]
-        for section in section_imports:
-            section.sort()
+        [section.sort() for section in section_imports]
         self.import_cache = section_imports
-        return
+        return None
     
     def construct_new_imports(self):
-        imports = self.import_cache
-        self.import_cache = str()
-        for section in imports:
-            for imp in section:
-                self.import_cache += f"{imp[1][0]}\n"
-            self.import_cache += "\n"
-        return
+        imports: str = "\n".join([imp[1][0] for imp in section for section in self.import_cache])
+        self.import_cache = imports + "\n\n"
+        return None
 
-print(ImportParser("formatter/import_sort/parse.py").parse())
+if __name__ == "__main__":
+    print(ImportParser("formatter/import_sort/parse.py").parse())
